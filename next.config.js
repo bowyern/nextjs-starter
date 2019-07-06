c72b85939faa2df69b0e1d4ec86b204d7d9b00ef
nbowyer <nbowyer@seredan.com>
2019-07-06 13:25:51 -0500

    Add a defaultExtractor that includes Tailwind responsive styles Add a whitelist to prevent PurgeCSS from removing some default tailwind styles

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withPurgeCss = require('next-purgecss')
  
 module.exports = withCss(
   withPurgeCss({
-    purgeCssEnabled: ({ dev, isServer }) => (!dev) // Only enable PurgeCSS for production builds
+    purgeCssEnabled: ({ dev, isServer }) => (!dev), // Only enable PurgeCSS for production builds
+    purgeCss: {
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      // Next doesn't use html or body tags anywhere by default.
+      // Whitelist them here so PurgeCSS doesn't remove
+      // some of the default Tailwind styles.
+      whitelist: () => ['html', 'body']
+    }
   })
 )
